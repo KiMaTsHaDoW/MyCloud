c0bc45699f2be02594682ea7ed485dd9b6975564
unknown <daviiiid.15@gmail.com>
2025-10-29 08:14:42 +0100

    inicio practica DAW

---
+++ Grado/DAW/practica_ut2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+/**
+* @brief Suma dos enteros
+* 
+* @param a Primer número
+* @param b Segundo número
+* @return int Resultado de la suma
+*/
+int sumar(int a, int b) {
+   return a + b;
+}
+int main() {
+   int x = 5, y = 7;
+   std::cout << "La suma es: " << sumar(x, y) << std::endl;
+   return 0;
+}
